29f90c605f92123c2db0c1012c06ae59822c8fbc
Aura <Aura.cl>
2017-03-06 10:38:11 -0300

    Alpha 1

--- AuraInventarioProto/Controllers/HomeController.cs
+++ AuraInventarioProto/Controllers/HomeController.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace AuraInventarioProto.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace AuraInventarioProto.Controllers {
+    public class HomeController : Controller {
         // GET: Home
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
+
+        //public ActionResult NotFound(string aspxerrorpath) {
+        //    if (!string.IsNullOrWhiteSpace(aspxerrorpath))
+        //        return RedirectToAction("NotFound");
+
+        //    return View();
+        //}
     }
 }
